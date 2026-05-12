1cdb97bd93e160a9b83c3804d3a98a6ad295e2f2
Yusuf Esan <yusufesan@Yusuf-MacBook-Pro.local>
2026-05-12 09:56:45 +0300

    chore: configure project for GitHub Pages deployment with automated workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   devIndicators: false,
-}
+};
 
 export default nextConfig;
